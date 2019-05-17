e3c16de04c1925c222155e94edb6b89aeeedb7f1
Erwin Rottler <rottler@uni-potsdam.de>
2019-05-17 13:48:39 +0200

    Add function to calculate mean in squared buffer.

---
+++ R/elev_buff.R
@@ -0,0 +1,32 @@
+#' Buffer mean value
+#'
+#' Get mean elevation of squared buffer around grid point.
+#'
+#' @param point_in  Spatial point
+#' @param radius  Length side of square buffer
+#' @param dem_in DEM
+#' @export
+elev_buff <- function(point_in, radius = 2500, dem_in){
+
+  y_plu <-point_in@bbox[2,1] + radius
+  x_plu <-point_in@bbox[1,1] + radius
+  y_min <-point_in@bbox[2,1] - radius
+  x_min <-point_in@bbox[1,1] - radius
+
+  square <- cbind(x_min, y_plu, #NW corner
+                  x_plu, y_plu, #NE corner
+                  x_plu, y_min, #SW corner
+                  x_min, y_min, #SW corner
+                  x_min, y_plu) #NW corner again to close polygon
+
+  pol_sq <- Polygon(matrix(square, ncol = 2, byrow = T))
+  sq_buf <- SpatialPolygons(list(Polygons(list(pol_sq), ID = "sq_buf")), proj4string = CRS(crs(basin, asText = T)))
+
+
+  #get mean elevation in squared buffer...
+
+  elev_square <- raster::extract(dem, sq_buf, fun = mean, na.rm = T)
+
+  return(elev_square)
+
+}
