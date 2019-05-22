7baa4c57dc9c9ad6dfc750be0330b2aadc35bba4
Erwin Rottler <rottler@uni-potsdam.de>
2019-05-22 12:27:54 +0200

    Upate read.grdc() - Make sure NAs recognized.

--- R/read_grdc.R
+++ R/read_grdc.R
@@ -6,7 +6,7 @@
 #' @export
 read_grdc <- function(file_path){
 
-  data_grdc <- read.table(file_path, sep = ";", header = T, stringsAsFactors = F, na.strings = "-999.000")
+  data_grdc <- read.table(file_path, sep = ";", header = T, stringsAsFactors = F, na.strings = c("-999.00","-999.000"))
 
   #define date
   data_grdc_date <- as.Date(strptime(data_grdc$YYYY.MM.DD, "%Y-%m-%d", tz="UTC"))
@@ -15,7 +15,11 @@ read_grdc <- function(file_path){
   data_grdc_value <- data_grdc$Value
 
   data_grdc <- data.frame(date  = data_grdc_date,
-                             value = data_grdc_value)
+                          value = data_grdc_value)
+
+  #in case na strings not recognized
+  na_vals <- which(data_grdc$value == -999)
+  data_grdc$value[na_vals] <- NA
 
   return(data_grdc)
 
